d3fd2638face2f600a0d7657f7f45b02c389b47b
joshlgrossman <josh.l.grossman@gmail.com>
2017-05-23 15:03:17 -0500

    Update template.js

--- template.js/template.js
+++ template.js/template.js
@@ -1,8 +1,12 @@
-function template(str, onChange){
+function template(o, f){
+  var str = typeof o === 'string' ? o : o.template;
+  var change = f || o.change;
+  var throttle = o.throttle;
 
-  var callbacks = onChange ? [onChange] : [];
+  var callbacks = change ? [change] : [];
   var re = /\#\{(\s*[a-zA-Z_$][a-zA-Z_$0-9]*\s*(\|[^}]*)?)\}/g;
   var vars = str.match(re);
+  var timeout;
   var obj = function(arg){
     if(typeof arg === 'function')
       callbacks.push(arg);
@@ -11,29 +15,31 @@ function template(str, onChange){
     return obj;
   };
 
-  function change(){
+  function _change(){
+    timeout = undefined;
     for(var i = 0; i < callbacks.length; i++)
       callbacks[i](obj.compiled);
   }
 
-  function parse(v){
+  function _parse(v){
     return v.replace(re,'$1').split('|').map(function(part){ return part.trim() });
   }
 
-  function prop(def){
+  function _prop(def){
     var val = def || '';
     return function(newVal){
-      if(newVal !== undefined) {
+      if(newVal !== undefined && newVal !== val) {
         val = newVal || def;
-        change();
+        if(throttle) !timeout && (timeout = setTimeout(_change, 0));
+        else _change();
       }
       return val;
     }
   }
 
   for(var v in vars){
-    var arr = parse(vars[v]);
-    var p = prop(arr[1]);
+    var arr = _parse(vars[v]);
+    var p = _prop(arr[1]);
     Object.defineProperty(obj, arr[0], {get:p, set:p});
   }
 
@@ -41,14 +47,14 @@ function template(str, onChange){
     get: function(){
       var res = str;
       for(var v in vars){
-        var val = obj[parse(vars[v])[0]];
+        var val = obj[_parse(vars[v])[0]];
         res = res.replace(vars[v], val);
       }
       return res;
     }
   });
 
-  change();
+  _change();
 
   return obj;
 
